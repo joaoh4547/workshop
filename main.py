03b37e0118174346c9908d2885f07764d86be6c9
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2021-03-02 12:18:18 -0300

    add download stop words

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from flask import request, jsonify
 from json import loads
-
+import nltk
 from sklearn.model_selection import train_test_split
 
 from ml.classifier import NeuralNetwork
 from ml import pre_process_data, Tokenize
 from utils import read_files_of_directory
-
+nltk.download("stopwords")
 file_data = read_files_of_directory('data')
 data = []
 labels = []
@@ -20,7 +20,8 @@ for x in file_data:
 data = pre_process_data(data)
 
 nn = NeuralNetwork()
-X_train, X_test, y_train, y_test = train_test_split(data, labels, train_size=0.9, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, labels, train_size=0.9, shuffle=True)
 tokenizer = Tokenize()
 features = tokenizer.test_vectorizer(X_train)
 nn.train(features, y_train)
